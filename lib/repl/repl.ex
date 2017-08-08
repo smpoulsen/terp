f6001b083c0223920407d62520b54bef2807c97c
Travis Poulsen <travis.poulsen@gmail.com>
2017-08-08 18:45:49 -0400

    Add color to repl

--- lib/repl/repl.ex
+++ lib/repl/repl.ex
@@ -9,9 +9,21 @@ defmodule Terp.Repl do
       :eof -> IO.write("\nBye!")
       _ ->
         {res, env} = Terp.evaluate_source(expr, environment)
-        IO.write(res)
-        IO.write("\n")
+        case res do
+          {:error, {type, msg}} ->
+            Bunt.puts([:red, "There was an error:"])
+            IO.puts("\tmsg: #{Atom.to_string(type)}")
+            IO.puts("\targ: #{msg}")
+          nil ->
+            Bunt.puts([:green, "ok"])
+          _ ->
+            IO.puts(res)
+        end
         loop(env)
     end
+  end
+
+  def track_history() do
+    # use a zipper to track history.
   end
 end
