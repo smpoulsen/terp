048c54f85d6bc2022b5656133cf41fb550a8b45e
Travis Poulsen <travis.poulsen@gmail.com>
2017-08-27 15:04:15 -0400

    Removes Zipper based history from REPL
    The history wasn't working as planned, and co-opting IEx works fine for my
    current purposes.

--- lib/repl/repl.ex
+++ lib/repl/repl.ex
@@ -2,30 +2,18 @@ defmodule Terp.Repl do
   @moduledoc """
   A REPL (read-eval-print-loop) for terp.
   """
-  alias RoseTree.Zipper
   alias Terp.Types.Types
   alias Terp.Types.TypeEnvironment
 
   def init() do
     # Starts a persisted type environment for the current session.
     TypeEnvironment.start_link()
-    loop("", fn (z) -> {:error, {:unbound_variable, z}} end, init_history(RoseTree.new(:start)))
+    loop(fn (z) -> {:error, {:unbound_variable, z}} end)
   end
 
-  def loop(init_expr, environment, history) do
-    if init_expr == "" do
-      :io.put_chars(init_expr)
-    end
+  def loop(environment) do
     expr = IO.gets("terp> ")
     case expr do
-      <<2>> ->
-        # Ctl-B
-        {previous, new_history} = scroll_back(history)
-        loop(previous, environment, new_history)
-      <<6>> ->
-        # Ctl-F
-        {next, new_history} = scroll_forward(history)
-        loop(next, environment, new_history)
       :eof ->
         # Ctl-D
         IO.write("\nBye!")
@@ -33,11 +21,10 @@ defmodule Terp.Repl do
         cond do
           String.starts_with?(expr, ":t ") || String.starts_with?(expr, ":type ") ->
             type_check(expr)
-            loop("", environment, history)
+            loop(environment)
           true ->
-            updated_history = add_history(expr, history)
             env =  eval(expr, environment)
-            loop("", env, updated_history)
+            loop(env)
         end
     end
   end
@@ -92,54 +79,6 @@ defmodule Terp.Repl do
       {:error, {module, reason}} ->
         Bunt.puts([:red, "#{to_string(module)} error:"])
       Bunt.puts([:yellow, "\t#{reason}"])
-    end
-  end
-
-  ### History ###
-  # History is implemented as a 1-ary rose tree.
-
-  # Initialize with an empty tree
-  defp init_history(item) do
-    item
-    |> RoseTree.new()
-    |> Zipper.from_tree()
-  end
-
-  # Insert new item into history.
-  defp add_history(new_item, history) do
-    # use a zipper to track history.
-    {:ok, updated} = history
-    |> Zipper.insert_first_child(RoseTree.new(String.trim(new_item)))
-    updated
-  end
-
-  # Scroll backwards through history. Wraps around if at the top.
-  defp scroll_back(history) do
-    with {:ok, previous} <- Zipper.ascend(history),
-         tree <- Zipper.to_tree(previous) do
-    {tree.node, history}
-    else
-      {:error, {:rose_tree, :no_parent}} ->
-        last = history
-        |> Zipper.to_leaf()
-        |> Zipper.to_tree()
-        {last.node, history}
-    {:error, e} -> e
-    end
-  end
-
-  # Scroll forwards through history. Wraps around if at the bottom.
-  defp scroll_forward(history) do
-    with {:ok, next} <- Zipper.first_child(history),
-         tree <- Zipper.to_tree(next) do
-      {tree.node, history}
-    else
-      {:error, {:rose_tree, :bad_path}} ->
-        first = history
-        |> Zipper.to_root()
-        |> Zipper.to_tree()
-      {first.node, history}
-    {:error, e} -> e
     end
   end
 end
